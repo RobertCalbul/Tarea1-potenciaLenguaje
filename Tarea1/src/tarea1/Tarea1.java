357b97b345905af8c6a430daa169c6e678c506ab
TheKillero001 <rcalbul2011@alu.uct.cl>
2014-08-20 02:33:57 -0400

    avance
    Algoritmo funcionando
    -Falta interfaz grafica

---
+++ Tarea1/src/tarea1/Tarea1.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea1;
+
+/**
+ *
+ * @author TheKiller
+ */
+public class Tarea1 {
+
+    private static int nMin = 1;
+    private static int nMax = 11;
+    private static String[] a1 = {"1", "0", "a", "b", "c"};
+    private static String[] a2 = new String[0];//alfabeto vacio
+    private static String[] l = {"11", "00", "ab", "aabbcc"};
+    private static String[] lAux = new String[0];
+
+    public static void main(String[] args) {
+        String[] retornar = null;
+        for (int i = nMin; i <= nMax; i++) {
+            System.out.println("Pasada -> " + i);
+            //tamaño de arreglo si L^2 = lenguaje*alfabeto si L^n donde n >2 entonces auxuliar * alfabeto
+            int size = i == 1 ? l.length : i == 2 ? (a1.length * l.length) : (lAux.length * a1.length);
+            String[] arr = lAux;
+            lAux = new String[size];
+            String[] arreglo2 = i == 1 ? l : arr;
+            int contador = 0;
+            for (int a = 0; a < arreglo2.length; a++) {
+                if (i == 1) {
+                    lAux[a] = l[a];
+                    System.out.println("valor: " + lAux[a]);
+                } else {
+                    
+                    for (int L = 0; L < a1.length; L++) {
+                        lAux[contador] = arreglo2[a].concat(a1[L]);//i<=1?l[a].concat(a1[L]): arr[contador].concat(a1[L]);
+                        System.out.println("valor: " + lAux[contador]);
+                        contador++;
+                    }
+                }
+            }
+        }
+    }
+}
